adc91e96b93b1f07d4517156e5e5f5bbb97c55e7
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-09-25 18:13:39 +0300

    added hero section without images

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,18 @@
+import Founder from "@/components/homePage/founder/Founder";
+import Hero from "@/components/homePage/hero/Hero";
+import CodeSiteMarquee from "@/components/shared/marquee/CodeSiteMarquee";
+
 export default function HomePage() {
-  return <></>;
+  return (
+    <>
+      <div className="md:hidden">
+        <CodeSiteMarquee className="w-[240vw] h-[32px] text-[31px] rotate-[4.369deg] bg-[linear-gradient(90deg,#D9E5FF_10.2%,#268DF4_59.69%)]" />
+      </div>
+      <Hero />
+      <div className="hidden md:block">
+        <CodeSiteMarquee className="h-[50px] text-[48px] rotate-0 bg-[linear-gradient(90deg,#D9E5FF_30.2%,#268DF4_59.69%)]" />
+      </div>
+      <Founder />
+    </>
+  );
 }

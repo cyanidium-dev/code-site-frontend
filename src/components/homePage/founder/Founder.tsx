85856490b95ff7e140a92fd66763d230679cec26
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-10-07 13:30:37 +0300

    updated founder section

--- src/components/homePage/founder/Founder.tsx
+++ src/components/homePage/founder/Founder.tsx
@@ -1,13 +1,14 @@
 import Container from "@/components/shared/container/Container";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
+import BenefitsList from "./BenefitsList";
 
 export default function Founder() {
   const t = useTranslations("homePage.founder");
 
   return (
     <section className="pt-30 xl:pt-[130px]">
-      <Container className="md:flex gap-[19px]">
+      <Container className="flex flex-col md:flex-row gap-12 md:gap-5">
         <div className="relative rounded-[8px] overflow-hidden w-full md:w-[calc(100%-19px-334px)] h-[514px]">
           <Image
             src="/images/homePage/founder/founder.webp"
@@ -17,32 +18,11 @@ export default function Founder() {
           />
         </div>
 
-        <div className="relative z-10 hidden md:flex flex-col justify-between w-[334px] h-auto px-8 pt-11 pb-[25px] rounded-[8px] overflow-hidden bg-[linear-gradient(180deg,#61C5FD_0%,#6D92FE_100%)]">
-          <p className="font-actay text-[32px] font-bold leading-[108%] uppercase">
+        <div className="md:w-[333px]">
+          <h2 className="mb-8 font-actay text-[22px] font-bold leading-[108%] uppercase">
             {t("actualSolutions")}
-          </p>
-          <p className="relative z-10 font-guano-apes text-[61px] uppercase leading-none">
-            {t("designOne")}
-            <br />
-            <span className=" text-transparent bg-clip-text bg-[linear-gradient(90deg,_#ffffff_0%,_#F671C8_100%)]">
-              {" "}
-              {t("designTwo")}
-            </span>
-          </p>
-          <Image
-            src="/images/homePage/founder/figure.webp"
-            alt="figure"
-            width={240}
-            height={380}
-            className="absolute right-0 bottom-0 mix-blend-darken"
-          />
-          <Image
-            src="/images/homePage/founder/drops.svg"
-            alt="figure"
-            width="184"
-            height="350"
-            className="absolute z-5 right-0 bottom-[23px]"
-          />
+          </h2>
+          <BenefitsList />
         </div>
       </Container>
     </section>
--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
     <header className="fixed top-0 left-0 z-50 py-6 xl:py-10 w-dvw">
       <Container className="relative flex items-center justify-between">
         <div
-          className={`absolute -top-3 md:-top-6 left-0 w-full h-16 xl:h-17 rounded-full z-[-20] transition duration-500 ease-in-out ${
+          className={`absolute -top-3 md:-top-6 lg:-top-3 xl:-top-4 left-0 w-full h-16 xl:h-17 rounded-full z-[-20] transition duration-500 ease-in-out ${
             isScrolled
               ? "bg-black/30 backdrop-blur-md shadow-[inset_0px_2px_16px_rgba(255,255,255,0.25)]"
               : "bg-transparent"

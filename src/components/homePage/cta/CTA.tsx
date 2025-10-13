1f7bc6f864eac005ad389052ad33d1cb97a4d30f
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-10-13 15:03:10 +0300

    added texts

--- src/components/homePage/cta/CTA.tsx
+++ src/components/homePage/cta/CTA.tsx
@@ -1,5 +1,12 @@
+import Container from "@/components/shared/container/Container";
+import SectionTitle from "@/components/shared/titles/SectionTitle";
+
 export default function CTA() {
   return (
-    <section className="pt-[146px] lg:pt-[180px] pb-[127px] lg:pb-[139px]"></section>
+    <section className="pt-[146px] lg:pt-[180px] pb-[127px] lg:pb-[139px]">
+      <Container>
+        <SectionTitle></SectionTitle>
+      </Container>
+    </section>
   );
 }

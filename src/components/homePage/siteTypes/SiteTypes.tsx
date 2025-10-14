75272ce77fef35329f28c215f296ecea0def5a4e
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-10-14 11:43:58 +0300

    added list

--- src/components/homePage/siteTypes/SiteTypes.tsx
+++ src/components/homePage/siteTypes/SiteTypes.tsx
@@ -1,9 +1,19 @@
 import Container from "@/components/shared/container/Container";
+import SectionTitle from "@/components/shared/titles/SectionTitle";
+import { useTranslations } from "next-intl";
+import SiteTypesList from "./SiteTypesList";
 
 export default function SiteTypes() {
+  const t = useTranslations("homePage.siteTypes");
+
   return (
     <section className="pt-[289px] xl:pt-[285px]">
-      <Container></Container>
+      <Container>
+        <SectionTitle className="lg:max-w-[523px] lg:ml-[115px] mb-[432px] lg:mb-[95px] text-[64px] lg:text-[111px] font-bold leading-[108%] text-center lg:text-left">
+          {t("title")}
+        </SectionTitle>
+        <SiteTypesList />
+      </Container>
     </section>
   );
 }
---
+++ src/components/homePage/siteTypes/SiteTypesList.tsx
@@ -0,0 +1,96 @@
+import MainButton from "@/components/shared/buttons/MainButton";
+import FourPointsStarIcon from "@/components/shared/icons/FourPointsStarIcon";
+import StarIcon from "@/components/shared/icons/StarIcon";
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+
+export default function SiteTypesList() {
+  const t = useTranslations("homePage.siteTypes");
+
+  const siteTypesList = [
+    {
+      title: t("landing.title"),
+      list: [
+        t("landing.listOne"),
+        t("landing.listTwo"),
+        t("landing.listThree"),
+        t("landing.listFour"),
+        t("landing.listFive"),
+      ],
+      price: 800,
+    },
+    {
+      title: t("multipage.title"),
+      list: [
+        t("multipage.listOne"),
+        t("multipage.listTwo"),
+        t("multipage.listThree"),
+        t("multipage.listFour"),
+      ],
+      price: 1500,
+    },
+    {
+      title: t("e-commerce.title"),
+      list: [
+        t("e-commerce.listOne"),
+        t("e-commerce.listTwo"),
+        t("e-commerce.listThree"),
+        t("e-commerce.listFour"),
+        t("e-commerce.listFive"),
+      ],
+      price: 2500,
+    },
+  ];
+
+  return (
+    <ul>
+      {siteTypesList.map(({ title, list, price }, idx) => (
+        <li key={idx} className="p-9 lg:px-8 lg:py-13">
+          <h3 className="mb-6 font-actay text-[16px] lg:text-[22px] font-bold leading-[108%] uppercase">
+            {title}
+          </h3>
+          <p className="mb-6 lg:mb-3 font-actay text-[12px] lg:text-[16px] font-bold leading-[100%] uppercase">
+            {t("forWhom")}
+          </p>
+          <ul className="flex flex-col gap-[19px] lg:gap-3 mb-9 lg:mb-10">
+            {list.map((item, idx) => (
+              <li key={idx} className="flex gap-3 items-center">
+                <StarIcon className="shrink-0" />
+                <p className="text-[12px] lg:text-[14px] font-light leading-[122%]">
+                  {item}
+                </p>
+              </li>
+            ))}
+          </ul>
+          <MainButton
+            variant="pink"
+            className="mb-3 lg:mb-6 px-5 text-[10px] lg:text-[12px]"
+          >
+            <div className="flex items-center justify-between w-full">
+              {t("button")}
+              <Image
+                src="/images/homePage/siteTypes/star.svg"
+                alt="star"
+                width={21}
+                height={21}
+                className="inline-block mb-1 rotate-45"
+              />
+            </div>
+          </MainButton>
+          <p className="w-fit ml-auto">
+            <span className="font-actay text-[16px] lg:text-[22px] font-bold leading-[120%] uppercase">
+              {t("from")}
+            </span>
+            &nbsp;&nbsp;
+            <span
+              className="font-guano-apes text-[40px] lg:text-[60px] font-medium leading-[108%] uppercase text-transparent bg-clip-text 
+            bg-gradient-to-r from-white to-[#F671C8]"
+            >
+              {price}$
+            </span>
+          </p>
+        </li>
+      ))}
+    </ul>
+  );
+}

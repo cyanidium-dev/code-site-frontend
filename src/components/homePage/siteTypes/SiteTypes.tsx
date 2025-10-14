7d5a3617b092c19dbb3c9924bb8c79403039f5bb
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-10-14 12:33:15 +0300

    added black bg

--- src/components/homePage/siteTypes/SiteTypes.tsx
+++ src/components/homePage/siteTypes/SiteTypes.tsx
@@ -7,7 +7,17 @@ export default function SiteTypes() {
   const t = useTranslations("homePage.siteTypes");
 
   return (
-    <section className="pt-[289px] xl:pt-[285px]">
+    <section className="relative z-10 pt-[289px] xl:pt-[285px]">
+      <div className="absolute -z-50 -top-3 left-0 inset-0 bg-black" />
+      <div
+        className="hidden lg:block absolute -z-40 -top-3 left-0 w-full h-[1024px] lg:h-[755px] opacity-30"
+        style={{
+          backgroundImage: "url(/images/homePage/cta/texture.png)",
+          backgroundRepeat: "repeat-x",
+          backgroundSize: "auto 100%",
+          backgroundPosition: "left top",
+        }}
+      />
       <Container>
         <SectionTitle className="lg:max-w-[523px] lg:ml-[115px] mb-[432px] lg:mb-[95px] text-[64px] lg:text-[111px] font-bold leading-[108%] text-center lg:text-left">
           {t("title")}

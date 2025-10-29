0e4a590fc90a3be665c096fe798f75ccbd8d78cc
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-10-29 18:58:27 +0200

    added decorations for desktop

--- src/components/homePage/portfolio/PortfolioDecorations.tsx
+++ src/components/homePage/portfolio/PortfolioDecorations.tsx
@@ -42,9 +42,10 @@ export default function PortfolioDecorations() {
           />
         </motion.div>
       </motion.div>
+
       <motion.div
         //  style={{ y: fastY }}
-        className="absolute -z-10 top-[53px] left-[-338px] w-[650px] lg:w-[908px] h-auto aspect-[1817/1770] bg-black"
+        className="absolute -z-10 top-[53px] lg:top-19 left-[-338px] lg:left-auto lg:right-[-148px] w-[650px] lg:w-[763px] h-auto aspect-[1817/1770] bg-black"
       >
         <motion.div
           initial="hidden"
@@ -63,6 +64,52 @@ export default function PortfolioDecorations() {
           />
         </motion.div>
       </motion.div>
+
+      <motion.div
+        //  style={{ y: fastY }}
+        className="hidden lg:block absolute -z-10 top-[170px] left-[-56px] w-[266px] h-auto aspect-[266/303]"
+      >
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.1 }}
+          variants={fadeInAnimation({ delay: 0.3, scale: 0.95 })}
+        >
+          <Image
+            src="/images/homePage/portfolio/drops.svg"
+            alt="drops"
+            width="266"
+            height="303"
+            className="w-[266px] h-auto"
+          />
+        </motion.div>
+      </motion.div>
+
+      <motion.div
+        //  style={{ y: fastY }}
+        className="hidden lg:block absolute -z-10 top-[308px] left-[-119px] w-[496px] h-auto aspect-[266/303]"
+      >
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.1 }}
+          variants={fadeInAnimation({ delay: 0.3, scale: 0.95 })}
+        >
+          <Image
+            src="/images/homePage/portfolio/code-site-desk.svg"
+            alt="code-site.art"
+            width="496"
+            height="552"
+            className="w-[496px] h-auto"
+          />
+        </motion.div>
+      </motion.div>
+
+      <div className="hidden lg:block absolute top-[262px] right-[-101px] w-[1026px] h-[586px] rotate-[-148deg] rounded-full bg-black supports-[backdrop-filter]:blur-[72px] will-change-transform" />
+
+      <div className="hidden lg:block absolute top-[322px] right-[-121px] w-[354px] h-[380px] rounded-full bg-main supports-[backdrop-filter]:blur-[281px] will-change-transform" />
     </div>
   );
 }

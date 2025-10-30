64c7bda40d0aba42837d5aa707f18bdaf92bcefb
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-10-30 16:25:29 +0200

    added images for desktop

--- src/components/blogPage/hero/HeroDecorations.tsx
+++ src/components/blogPage/hero/HeroDecorations.tsx
@@ -26,7 +26,7 @@ export default function HeroDecorations() {
     <div ref={sectionRef} className="absolute inset-0 pointer-events-none">
       <motion.div
         //    style={{ y: slowY }}
-        className="absolute top-[344px] left-[calc(50%-177px)] w-[353px] h-auto aspect-[353/530]"
+        className="absolute top-[344px] lg:top-15 left-[calc(50%-177px)] lg:left-[calc(50%-97px)] w-[353px] h-auto aspect-[353/530]"
       >
         <motion.div
           initial="hidden"
@@ -47,7 +47,7 @@ export default function HeroDecorations() {
 
       <motion.div
         //    style={{ y: slowY }}
-        className="absolute top-[115px] right-[calc(50%-180px)] w-[164px] lg:w-[244px] h-auto aspect-[244/235]"
+        className="absolute top-[115px] lg:top-[101px] right-[calc(50%-180px)] lg:right-[calc(50%-701px)] w-[164px] lg:w-[244px] h-auto aspect-[244/235]"
       >
         <motion.div
           initial="hidden"
@@ -68,7 +68,28 @@ export default function HeroDecorations() {
 
       <motion.div
         //    style={{ y: slowY }}
-        className="absolute -z-10 top-[385px] right-[calc(50%-213px)] w-[255px] lg:w-[365px] h-auto aspect-[365/283]"
+        className="hidden lg:block absolute z-20 top-[328px] left-[calc(50%-290px)] w-[224px] h-auto aspect-[224/252]"
+      >
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.1 }}
+          variants={fadeInAnimation({ delay: 0.3, scale: 0.95 })}
+        >
+          <Image
+            src="/images/blogPage/hero/drops-pink.svg"
+            alt="blue drops"
+            width="224"
+            height="252"
+            className="w-[224px] h-auto"
+          />
+        </motion.div>
+      </motion.div>
+
+      <motion.div
+        //    style={{ y: slowY }}
+        className="absolute -z-10 top-[385px] lg:top-[318px] right-[calc(50%-213px)] lg:right-auto lg:left-[calc(50%-625px)] w-[255px] lg:w-[365px] h-auto aspect-[365/283]"
       >
         <motion.div
           initial="hidden"
@@ -87,9 +108,12 @@ export default function HeroDecorations() {
         </motion.div>
       </motion.div>
 
-      <div className="absolute -z-30 top-[373px] left-[calc(50%-337px)] w-[469px] h-[420px] rounded-full bg-purple-bright supports-[backdrop-filter]:blur-[164px] will-change-transform" />
-      <div className="absolute -z-20 top-[-51px] left-[calc(50%-400px)] w-[697px] h-[392px] rounded-full bg-black supports-[backdrop-filter]:blur-[176.7px] will-change-transform" />
-      <div className="absolute z-10 bottom-[-168px] left-[calc(50%-371px)] w-[194%] h-[372px] rounded-full bg-black supports-[backdrop-filter]:blur-[90px] will-change-transform" />
+      <div className="absolute -z-30 top-[373px] lg:top-[-288px] left-[calc(50%-337px)] lg:left-[calc(50%-243px)] w-[469px] h-[420px] rounded-full bg-purple-bright supports-[backdrop-filter]:blur-[164px] will-change-transform" />
+      <div className="lg:hidden absolute -z-20 top-[-51px] left-[calc(50%-400px)] w-[697px] h-[392px] rounded-full bg-black supports-[backdrop-filter]:blur-[176.7px] will-change-transform" />
+      <div
+        className="absolute z-10 bottom-[-168px] lg:bottom-[-330px] left-[calc(-53%)] lg:left-[calc(50%-1019px)] w-[194%] lg:w-[2564px] h-[372px]
+       lg:h-[699px] rounded-full bg-black supports-[backdrop-filter]:blur-[90px] lg:supports-[backdrop-filter]:blur-[106px] will-change-transform"
+      />
     </div>
   );
 }

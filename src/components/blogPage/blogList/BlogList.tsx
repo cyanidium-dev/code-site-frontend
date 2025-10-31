430e9acd75434bb60d83ce05601a056f5c1c3073
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-10-31 15:20:20 +0200

    added pagination

--- src/components/blogPage/blogList/BlogList.tsx
+++ src/components/blogPage/blogList/BlogList.tsx
@@ -6,13 +6,15 @@ interface BlogListProps {
   blogList: Blog[];
 }
 
+const SECTION_ID = "blog-page-posts-list";
+
 export default function BlogList({ blogList }: BlogListProps) {
   if (!blogList || !blogList?.length) return null;
 
   console.log(blogList);
 
   return (
-    <section className="relative z-10">
+    <section id={SECTION_ID} className="relative z-10">
       <Container className="pt-[50px] lg:pt-0 pb-[413px] lg:pb-[431px]">
         <ArticlesList blogList={blogList} />
       </Container>

35ec222baefe81a337250f8a6a79449917d0ce2f
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-10-31 13:30:53 +0200

    added all blogs query

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,6 +1,18 @@
 import Hero from "@/components/blogPage/hero/Hero";
+import { allBlogsQuery } from "@/lib/queries";
+import { fetchSanityData } from "@/utils/fetchSanityData";
 
-export default function BlogPage() {
+interface BlogPageProps {
+  params: Promise<{ locale: Locale }>;
+}
+
+export default async function BlogPage({ params }: BlogPageProps) {
+  const { locale } = await params;
+
+  const blogList = await fetchSanityData(allBlogsQuery, {
+    lang: locale,
+  });
+
   return (
     <>
       <Hero />

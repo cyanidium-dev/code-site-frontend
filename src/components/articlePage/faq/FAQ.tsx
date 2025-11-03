fe475757f15a9e81d3e1d1df6b6dbf889c42baf0
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-11-03 13:04:37 +0200

    added faq section

---
+++ src/components/articlePage/faq/FAQ.tsx
@@ -0,0 +1,22 @@
+import Container from "@/components/shared/container/Container";
+import FaqList from "@/components/shared/faqList/FaqList";
+import SectionTitle from "@/components/shared/titles/SectionTitle";
+import { useTranslations } from "next-intl";
+
+export default function FAQ() {
+  const t = useTranslations("homePage.faq");
+
+  return (
+    <section>
+      <Container>
+        <SectionTitle
+          variant="pink"
+          className="mb-8 lg:mb-10 text-[24px] lg:text-[32px] font-bold leading-none"
+        >
+          {t("title")}
+        </SectionTitle>
+        <FaqList />
+      </Container>
+    </section>
+  );
+}

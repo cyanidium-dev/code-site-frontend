f8da1daa3ebcc3a11fb9ca68ae7b52d73e53e1da
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-11-07 12:56:14 +0200

    updated render splash screen

--- src/components/shared/splashScreen/LottieSplashScreen.tsx
+++ src/components/shared/splashScreen/LottieSplashScreen.tsx
@@ -1,41 +1,22 @@
 "use client";
 
-import { AnimatePresence, motion } from "framer-motion";
 import Lottie from "lottie-react";
 
 import animationData from "./lottieLogoAnimation.json";
 
-interface LottieSplashScreenProps {
-  visible: boolean;
-}
-
-export default function LottieSplashScreen({
-  visible,
-}: LottieSplashScreenProps) {
+export default function LottieSplashScreen() {
   return (
-    <AnimatePresence>
-      {visible && (
-        <motion.div
-          initial={{ opacity: 1 }}
-          exit={{
-            opacity: 0,
-            transition: { duration: 1, ease: "easeInOut" },
-          }}
-          transition={{ duration: 1 }}
-          className="no-doc-scroll fixed inset-0 z-50 flex items-center justify-center bg-[#020418]"
-        >
-          <Lottie
-            animationData={animationData}
-            loop={false}
-            autoplay
-            style={{
-              width: 300,
-              height: 300,
-              position: "absolute",
-            }}
-          />
-        </motion.div>
-      )}
-    </AnimatePresence>
+    <div className="no-doc-scroll fixed inset-0 z-[9999] flex items-center justify-center bg-[#020418]">
+      <Lottie
+        animationData={animationData}
+        loop={false}
+        autoplay
+        style={{
+          width: 300,
+          height: 300,
+          position: "absolute",
+        }}
+      />
+    </div>
   );
 }

cd358297705118c24bc1b104bcbdd5450c503371
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-11-07 10:42:59 +0200

    deleted animation

--- src/components/homePage/hero/BenefitItem.tsx
+++ src/components/homePage/hero/BenefitItem.tsx
@@ -1,13 +1,4 @@
 "use client";
-import { useEffect, useRef } from "react";
-import {
-  motion,
-  useMotionValue,
-  useTransform,
-  animate,
-  useInView,
-} from "framer-motion";
-import { listItemVariants } from "@/utils/animationVariants";
 
 interface BenefitItemProps {
   benefit: {
@@ -19,40 +10,14 @@ interface BenefitItemProps {
 export default function BenefitItem({ benefit }: BenefitItemProps) {
   const { value, description } = benefit;
 
-  const number = parseFloat(value.replace(/[^\d.]/g, ""));
-  const suffix = value.replace(/[\d.]/g, "");
-
-  const count = useMotionValue(0);
-  const rounded = useTransform(count, (latest) =>
-    value.includes(".") ? latest.toFixed(1) : Math.round(latest).toString()
-  );
-
-  const ref = useRef(null);
-  const inView = useInView(ref, { once: true, amount: 0.2 });
-
-  useEffect(() => {
-    if (inView) {
-      const controls = animate(count, number, {
-        duration: 3.8,
-        ease: "easeOut",
-      });
-      return () => controls.stop();
-    }
-  }, [inView, count, number]);
-
   return (
-    <motion.li
-      ref={ref}
-      variants={listItemVariants}
-      className="flex flex-col justify-center items-center w-[calc(50%-5px)] lg:w-[186px] xl:w-[246px] pt-1.5 pb-3 lg:pb-[15px] px-4 rounded-[4px] lg:rounded-[6px] border-[1.5px] lg:border-2 border-white"
-    >
+    <li className="flex flex-col justify-center items-center w-[calc(50%-5px)] lg:w-[186px] xl:w-[246px] pt-1.5 pb-3 lg:pb-[15px] px-4 rounded-[4px] lg:rounded-[6px] border-[1.5px] lg:border-2 border-white">
       <p className="mb-[5px] xl:mb-4 font-guano-apes text-[50px] lg:text-[70px] xl:text-[90px] font-medium uppercase">
-        <motion.span>{rounded}</motion.span>
-        {suffix}
+        {value}
       </p>
       <p className="font-actay text-[8px] lg:text-[10px] xl:text-[12px] font-bold uppercase text-center">
         {description}
       </p>
-    </motion.li>
+    </li>
   );
 }
--- src/components/homePage/hero/BenefitsList.tsx
+++ src/components/homePage/hero/BenefitsList.tsx
@@ -1,9 +1,5 @@
 "use client";
-import * as motion from "motion/react-client";
-import { listVariants, listItemVariantsLeft } from "@/utils/animationVariants";
 import BenefitItem from "./BenefitItem";
-import { useSplashScreen } from "@/hooks/useSplashScreen";
-import { getAnimationDelay } from "@/utils/getAnimationDelay";
 
 interface BenefitsListProps {
   list: { value: string; description: string }[];
@@ -16,62 +12,36 @@ export default function BenefitsList({
   subtitle,
   counterColor,
 }: BenefitsListProps) {
-  const isLoadingSplashScreen = useSplashScreen();
-  const delayChildren = getAnimationDelay(isLoadingSplashScreen, 0);
-
   return (
-    <motion.ul
-      key={`benefits-${isLoadingSplashScreen}`}
-      initial="hidden"
-      whileInView="visible"
-      exit="exit"
-      viewport={{ once: true, amount: 0.2 }}
-      variants={listVariants({ staggerChildren: 0, delayChildren })}
-      className="relative z-10 flex flex-wrap md:flex-nowrap gap-2.5 lg:gap-[15px] mt-4 lg:mt-18"
-    >
+    <ul className="relative z-10 flex flex-wrap md:flex-nowrap gap-2.5 lg:gap-[15px] mt-4 lg:mt-18">
       {list.map((benefit, idx) => (
         <BenefitItem key={idx} benefit={benefit} />
       ))}
-      <motion.li
-        variants={listItemVariantsLeft}
-        className="flex flex-col justify-center w-[calc(50%-5px)] lg:w-[210px] lg:mt-[-61px] lg:ml-[45px]"
-      >
+      <li className="flex flex-col justify-center w-[calc(50%-5px)] lg:w-[210px] lg:mt-[-61px] lg:ml-[45px]">
         <p className="lg:mb-4 xl:mb-9 font-actay text-[12px] lg:text-[16px] leading-[120%] font-bold uppercase">
           {subtitle}
         </p>
         <ul className="hidden lg:flex gap-[15px] text-[19px] font-normal leading-none">
           <li
-            className="flex justify-center items-center size-12 rounded-full border-2 benefit-highlight-1"
-            style={{
-              color: counterColor,
-              borderColor: counterColor,
-              animationDelay: isLoadingSplashScreen ? "0s" : "0s",
-            }}
+            className="flex justify-center items-center size-12 rounded-full border-2"
+            style={{ color: counterColor, borderColor: counterColor }}
           >
             01
           </li>
           <li
-            className="flex justify-center items-center size-12 rounded-full border-2 benefit-highlight-2"
-            style={{
-              color: counterColor,
-              borderColor: counterColor,
-              animationDelay: isLoadingSplashScreen ? "0s" : "0s",
-            }}
+            className="flex justify-center items-center size-12 rounded-full border-2"
+            style={{ color: counterColor, borderColor: counterColor }}
           >
             02
           </li>
           <li
-            className="flex justify-center items-center size-12 rounded-full border-2 benefit-highlight-3"
-            style={{
-              color: counterColor,
-              borderColor: counterColor,
-              animationDelay: isLoadingSplashScreen ? "0s" : "0s",
-            }}
+            className="flex justify-center items-center size-12 rounded-full border-2"
+            style={{ color: counterColor, borderColor: counterColor }}
           >
             03
           </li>
         </ul>
-      </motion.li>
-    </motion.ul>
+      </li>
+    </ul>
   );
 }

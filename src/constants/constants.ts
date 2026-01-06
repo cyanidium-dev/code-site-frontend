07ff2653f04788eb849a950315f7270782adabea
Shade <kulakaa073@gmail.com>
2026-01-06 20:33:04 +0200

    feat: updating social links

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,12 +3,12 @@ export const PHONE = "+380970068707";
 export const EMAIL = "hi@code-site.art";
 
 export const INSTAGRAM_NAME = "code-site.art";
-export const INSTAGRAM_LINK = "";
+export const INSTAGRAM_LINK = "https://www.instagram.com/cyanidium/";
 
 export const TELEGRAM_NAME = "@fedirdev";
 export const TELEGRAM_LINK = "https://t.me/fedirdev";
 
-export const LINKEDIN_NAME = "@yourusername";
-export const LINKEDIN_LINK = "";
+export const LINKEDIN_NAME = "@fediralpatov";
+export const LINKEDIN_LINK = "https://www.linkedin.com/in/fediralpatov/";
 
-export const TIKTOK_LINK = "";
+export const TIKTOK_LINK = "https://www.tiktok.com/@cyanidium.dev";

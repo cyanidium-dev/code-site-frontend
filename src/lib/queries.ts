f73bf579251c4508da0a3c1237694708c543c3c0
Shade <kulakaa073@gmail.com>
2026-01-07 18:29:53 +0200

    fix: leaving only [$lang] query

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -95,7 +95,7 @@ export const allProjectsQuery = `
       }
     },
     "websiteUrl": websiteUrl,
-    "advantages": coalesce(advantages[$lang], advantages),
+    "advantages": advantages[$lang],
     "portfolioTitle": portfolioTitle[$lang],
     "portfolioDescription": portfolioDescription[$lang],
     "order": order,

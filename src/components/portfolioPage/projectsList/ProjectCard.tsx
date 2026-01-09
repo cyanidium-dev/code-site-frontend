20da61e7a817d6bac4263b469d190f7b90c73585
Shade <kulakaa073@gmail.com>
2026-01-09 16:53:13 +0200

    fix: increasing project card title clamp to 3

--- src/components/portfolioPage/projectsList/ProjectCard.tsx
+++ src/components/portfolioPage/projectsList/ProjectCard.tsx
@@ -17,19 +17,28 @@ interface ProjectCardProps {
 export default function ProjectCard({ project, className }: ProjectCardProps) {
   const t = useTranslations("portfolioPage");
   console.log(project);
-  const { portfolioTitle, portfolioDescription, previewImage, slug, backgroundColor } = project;
-  
+  const {
+    portfolioTitle,
+    portfolioDescription,
+    previewImage,
+    slug,
+    backgroundColor,
+  } = project;
+
   const textColorClass = getTextColorClass(
     backgroundColor || "#000",
     "text-white",
     "text-black"
   );
-  
+
   return (
     <motion.li
       viewport={{ once: true, amount: 0.2 }}
       variants={listItemVariants}
-      className={twMerge("relative sm:w-[calc(50%-10px)] lg:w-[calc(33.33%-13.33px)] h-full rounded-[8px] overflow-hidden", className)}
+      className={twMerge(
+        "relative sm:w-[calc(50%-10px)] lg:w-[calc(33.33%-13.33px)] h-auto rounded-[8px] overflow-hidden",
+        className
+      )}
       style={{
         backgroundColor: backgroundColor || "#000",
       }}
@@ -37,7 +46,8 @@ export default function ProjectCard({ project, className }: ProjectCardProps) {
       <div
         className="absolute z-10 inset-0 rounded-[8px] pointer-events-none"
         style={{
-          background: "radial-gradient(78.44% 78.44% at 25.08% 14.06%, #EEF3FF 0%, #5B92D0 46%, #101A33 100%)",
+          background:
+            "radial-gradient(78.44% 78.44% at 25.08% 14.06%, #EEF3FF 0%, #5B92D0 46%, #101A33 100%)",
           padding: "1.5px",
           WebkitMask:
             "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
@@ -45,7 +55,7 @@ export default function ProjectCard({ project, className }: ProjectCardProps) {
           maskComposite: "exclude",
         }}
       />
-      <Link href={`/portfolio/${slug}`}>
+      <Link href={`/portfolio/${slug}`} className="flex flex-col h-full">
         <div className="relative px-[26px] py-4 w-full h-[222px] rounded-t-[8px] overflow-hidden">
           <Image
             src={previewImage?.asset?.url || ""}
@@ -55,28 +65,48 @@ export default function ProjectCard({ project, className }: ProjectCardProps) {
             className="object-cover"
           />
         </div>
-        <div className="p-5 xl:pb-8">
-          <h3 className={twMerge("min-h-[2lh] mb-4 font-actay text-[20px] font-bold leading-[120%] uppercase line-clamp-2", textColorClass)}>
+        <div className="p-5 xl:pb-8 flex flex-col justify-between flex-grow">
+          <h3
+            className={twMerge(
+              "mb-4 font-actay text-[20px] font-bold leading-[120%] uppercase line-clamp-3",
+              textColorClass
+            )}
+          >
             {portfolioTitle}
           </h3>
-          <p className={twMerge("min-h-[4lh] mb-8 text-[12px] lg:text-[14px] font-light leading-[120%] line-clamp-4", textColorClass)}>
-            {portfolioDescription}  
-          </p>
-          <MainButton 
-            variant="icon"
-            className={twMerge("h-[47px] text-[16px] text-raleway font-bold uppercase text-left p-5", getTextColorClass(backgroundColor || "#000", "bg-white text-black", "bg-black text-white"))}
-            icon={
-            <span 
-              className="absolute flex items-center justify-center right-[6px] size-9 top-1/2 -translate-y-1/2 rounded-full"
-              style={{
-                backgroundColor: backgroundColor || "#000",
-              }}
+          <div>
+            <p
+              className={twMerge(
+                "min-h-[4lh] mb-8 text-[12px] lg:text-[14px] font-light leading-[120%] line-clamp-4",
+                textColorClass
+              )}
             >
-              <ArrowIcon className={twMerge("size-5", textColorClass)} />
-            </span>
-            }
-            >{t("seeProject")}
-          </MainButton>
+              {portfolioDescription}
+            </p>
+            <MainButton
+              variant="icon"
+              className={twMerge(
+                "h-[47px] text-[16px] text-raleway font-bold uppercase text-left p-5",
+                getTextColorClass(
+                  backgroundColor || "#000",
+                  "bg-white text-black",
+                  "bg-black text-white"
+                )
+              )}
+              icon={
+                <span
+                  className="absolute flex items-center justify-center right-[6px] size-9 top-1/2 -translate-y-1/2 rounded-full"
+                  style={{
+                    backgroundColor: backgroundColor || "#000",
+                  }}
+                >
+                  <ArrowIcon className={twMerge("size-5", textColorClass)} />
+                </span>
+              }
+            >
+              {t("seeProject")}
+            </MainButton>
+          </div>
         </div>
       </Link>
     </motion.li>
